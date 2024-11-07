7adce6e12da9abd442b5d18aeea18bf56587ad2c
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-07 17:27:02 +0100

    ADD resto dell'esercizio

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const container = document.querySelector(".row");
+  const row = document.querySelector(".row");
+  const overlay = document.querySelector(".overlay");
+  const overlayImage = overlay.querySelector(".image-container img");
+  const closeOverlayButton = document.getElementById("close-overlay");
+
   function fetchPhotos() {
     axios
       .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
@@ -11,14 +15,32 @@ document.addEventListener("DOMContentLoaded", () => {
             <div class="col-4">
                 <div class="card">
                     <img class="pin" src="./img/pin.svg" alt="pin">
-                    <img src="${photo.thumbnailUrl}" alt="${photo.title}"> 
-                    <p>${photo.title}</p> 
+                    <img class="image thumbnail" src="${photo.url}" alt="${photo.title}" data-full-url="${photo.url}"> 
+                    <p class="caption">${photo.title}</p> 
                 </div>
             </div> `;
         });
-        container.innerHTML = photosHTML;
+        row.innerHTML = photosHTML;
+        addThumbnailClickEvent();
       })
       .catch((error) => console.error("Errore nella fetch delle foto:", error));
   }
+
+  function addThumbnailClickEvent() {
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    thumbnails.forEach((thumbnail) => {
+      thumbnail.addEventListener("click", () => {
+        const fullSizeUrl = thumbnail.getAttribute("data-full-url");
+        console.log(`URL immagine completa: ${fullSizeUrl}`);
+        overlayImage.src = fullSizeUrl;
+        overlay.classList.remove("d-none");
+      });
+    });
+  }
+
+  closeOverlayButton.addEventListener("click", () => {
+    overlay.classList.add("d-none");
+  });
+
   fetchPhotos();
 });
