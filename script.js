4cc54796a21f1631a5b9dfeaedf68857d237e3b2
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-10 12:39:15 +0100

    ADD aggiustamenti grafici al js

--- script.js
+++ script.js
@@ -1,3 +1,66 @@
+
+// Versione simile alle correzioni e più corretta:
+
+document.addEventListener("DOMContentLoaded", () => {
+  const row = document.querySelector(".row");
+  const overlay = document.querySelector(".overlay");
+  const overlayImage = overlay.querySelector(".image-container img");
+  const closeOverlayButton = document.getElementById("close-overlay");
+  
+  function toggleOverlay() {
+    overlay.classList.toggle("d-none");
+    document.body.classList.toggle("overflow-hidden");
+  }
+  
+  overlay.addEventListener("click", (event) => {
+    if (event.target.tagName !== "IMG") {
+      toggleOverlay();
+    }
+  });
+  
+  closeOverlayButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleOverlay();
+  });
+  
+  function fetchPhotos() {
+    axios
+    .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+    .then((response) => {
+      const photos = response.data;
+      let photosHTML = "";
+        photos.forEach((photo) => {
+          photosHTML += ` 
+            <div class="col-4">
+            <div class="card">
+                    <img class="pin" src="./img/pin.svg" alt="pin">
+                    <img class="image thumbnail" src="${photo.url}" alt="${photo.title}" data-full-url="${photo.url}"> 
+                    <p class="caption">${photo.title}</p> 
+                    </div>
+                    </div> `;
+        });
+        row.innerHTML += photosHTML;
+        addThumbnailClickEvent(photos); // Passa l'array photos alla funzione
+      })
+      .catch((error) => console.error("Errore nella fetch delle foto:", error));
+    }
+
+    function addThumbnailClickEvent(photos) {
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    thumbnails.forEach((thumbnail, index) => {
+      thumbnail.addEventListener("click", () => {
+        const fullSizeUrl = photos[index].url;
+        overlayImage.src = fullSizeUrl;
+        toggleOverlay();
+      });
+    });
+  }
+  
+  fetchPhotos();
+});
+
+// vecchia versione:
+
 // document.addEventListener("DOMContentLoaded", () => {
 //   const row = document.querySelector(".row");
 //   const overlay = document.querySelector(".overlay");
@@ -46,63 +109,3 @@
 
 //   fetchPhotos();
 // });
-
-//   // Versione simile alle correzioni e più corretta:
-
-document.addEventListener("DOMContentLoaded", () => {
-  const row = document.querySelector(".row");
-  const overlay = document.querySelector(".overlay");
-  const overlayImage = overlay.querySelector(".image-container img");
-  const closeOverlayButton = document.getElementById("close-overlay");
-
-  function toggleOverlay() {
-    overlay.classList.toggle("d-none");
-    document.body.classList.toggle("overflow-hidden");
-  }
-
-  overlay.addEventListener("click", (event) => {
-    if (event.target.tagName !== "IMG") {
-      toggleOverlay();
-    }
-  });
-
-  closeOverlayButton.addEventListener("click", (event) => {
-    event.stopPropagation();
-    toggleOverlay();
-  });
-
-  function fetchPhotos() {
-    axios
-      .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
-      .then((response) => {
-        const photos = response.data;
-        let photosHTML = "";
-        photos.forEach((photo) => {
-          photosHTML += ` 
-            <div class="col-4">
-                <div class="card">
-                    <img class="pin" src="./img/pin.svg" alt="pin">
-                    <img class="image thumbnail" src="${photo.url}" alt="${photo.title}" data-full-url="${photo.url}"> 
-                    <p class="caption">${photo.title}</p> 
-                </div>
-            </div> `;
-        });
-        row.innerHTML += photosHTML;
-        addThumbnailClickEvent(photos); // Passa l'array photos alla funzione
-      })
-      .catch((error) => console.error("Errore nella fetch delle foto:", error));
-  }
-
-  function addThumbnailClickEvent(photos) {
-    const thumbnails = document.querySelectorAll(".thumbnail");
-    thumbnails.forEach((thumbnail, index) => {
-      thumbnail.addEventListener("click", () => {
-        const fullSizeUrl = photos[index].url;
-        overlayImage.src = fullSizeUrl;
-        toggleOverlay();
-      });
-    });
-  }
-
-  fetchPhotos();
-});
