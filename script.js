f3cdbcc993263a87daf574d2c1da8c82be50d4bd
Lorenzo-Malatesta99 <lorenzo.ma932@gmail.com>
2024-11-10 12:38:02 +0100

    ADD nuova versione migliorata del js

--- script.js
+++ script.js
@@ -1,8 +1,75 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   const row = document.querySelector(".row");
+//   const overlay = document.querySelector(".overlay");
+//   const overlayImage = overlay.querySelector(".image-container img");
+//   const closeOverlayButton = document.getElementById("close-overlay");
+
+//   function fetchPhotos() {
+//     axios
+//       .get("https://jsonplaceholder.typicode.com/photos?_limit=6")
+//       .then((response) => {
+//         const photos = response.data;
+//         let photosHTML = "";
+//         photos.forEach((photo) => {
+//           photosHTML += `
+//             <div class="col-4">
+//                 <div class="card">
+//                     <img class="pin" src="./img/pin.svg" alt="pin">
+//                     <img class="image thumbnail" src="${photo.url}" alt="${photo.title}" data-full-url="${photo.url}">
+//                     <p class="caption">${photo.title}</p>
+//                 </div>
+//             </div> `;
+//         });
+//         row.innerHTML += photosHTML;
+//         addThumbnailClickEvent();
+//       })
+//       .catch((error) =>
+//         console.error("Errore nel caricamento delle foto", error)
+//       );
+//   }
+
+// function addThumbnailClickEvent() {
+//   const thumbnails = document.querySelectorAll(".thumbnail");
+//   thumbnails.forEach((thumbnail) => {
+//     thumbnail.addEventListener("click", () => {
+//       const fullSizeUrl = thumbnail.getAttribute("data-full-url");
+//       console.log(`URL immagine completa: ${fullSizeUrl}`);
+//       overlayImage.src = fullSizeUrl;
+//       overlay.classList.remove("d-none");
+//     });
+//   });
+// }
+
+// closeOverlayButton.addEventListener("click", () => {
+//   overlay.classList.add("d-none");
+// });
+
+//   fetchPhotos();
+// });
+
+//   // Versione simile alle correzioni e più corretta:
+
 document.addEventListener("DOMContentLoaded", () => {
   const row = document.querySelector(".row");
   const overlay = document.querySelector(".overlay");
   const overlayImage = overlay.querySelector(".image-container img");
   const closeOverlayButton = document.getElementById("close-overlay");
+
+  function toggleOverlay() {
+    overlay.classList.toggle("d-none");
+    document.body.classList.toggle("overflow-hidden");
+  }
+
+  overlay.addEventListener("click", (event) => {
+    if (event.target.tagName !== "IMG") {
+      toggleOverlay();
+    }
+  });
+
+  closeOverlayButton.addEventListener("click", (event) => {
+    event.stopPropagation();
+    toggleOverlay();
+  });
 
   function fetchPhotos() {
     axios
@@ -20,27 +87,22 @@ document.addEventListener("DOMContentLoaded", () => {
                 </div>
             </div> `;
         });
-        row.innerHTML = photosHTML;
-        addThumbnailClickEvent();
+        row.innerHTML += photosHTML;
+        addThumbnailClickEvent(photos); // Passa l'array photos alla funzione
       })
       .catch((error) => console.error("Errore nella fetch delle foto:", error));
   }
 
-  function addThumbnailClickEvent() {
+  function addThumbnailClickEvent(photos) {
     const thumbnails = document.querySelectorAll(".thumbnail");
-    thumbnails.forEach((thumbnail) => {
+    thumbnails.forEach((thumbnail, index) => {
       thumbnail.addEventListener("click", () => {
-        const fullSizeUrl = thumbnail.getAttribute("data-full-url");
-        console.log(`URL immagine completa: ${fullSizeUrl}`);
+        const fullSizeUrl = photos[index].url;
         overlayImage.src = fullSizeUrl;
-        overlay.classList.remove("d-none");
+        toggleOverlay();
       });
     });
   }
-
-  closeOverlayButton.addEventListener("click", () => {
-    overlay.classList.add("d-none");
-  });
 
   fetchPhotos();
 });
